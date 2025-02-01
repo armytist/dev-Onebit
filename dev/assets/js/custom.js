315883906675dc14d1089f48db5851164a4beee5
armytist <armybtslovebts3@gmail.com>
2025-02-01 23:34:16 +0200

    feat: show/hide password field

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('[type="tel"]').mask('+38(000)-000-0000');
+    $('#password').hideShowPassword({
+        innerToggle: true,
+    });
 })
+
+const btnHistoryBack= document.querySelector('#history-back');
+const backHistory =()=> window.history.back();
+btnHistoryBack.addEventListener('click', backHistory)
+
+
