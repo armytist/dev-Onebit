361ea7abdf8ecf71a5d69544a228809b229ad0fb
armytist <armybtslovebts3@gmail.com>
2025-02-04 01:50:47 +0200

    feat: fixed header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,8 +5,51 @@ $(document).ready(function(){
     });
 })
 
-const btnHistoryBack= document.querySelector('#history-back');
+/*const btnHistoryBack= document.querySelector('#history-back');
 const backHistory =()=> window.history.back();
-btnHistoryBack.addEventListener('click', backHistory)
+btnHistoryBack.addEventListener('click', backHistory);*/
+
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    if (window.scrollY > header.offsetTop) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+});
+
+const swiper = new swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // slidesPerView: 2,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+  });
+
+
+const time = 23
+  let greeting;
+
+  if (time < 3) {
+    greeting = "Good night!";
+  } else if (time < 11) {
+    greeting = "Good morning!"
+  } else if (time < 15) {
+    greeting = "Good day!"
+  } else if (time < 23) {
+    greeting = "Good evening";
+  } 
+console.log(greeting);
+
 
 
